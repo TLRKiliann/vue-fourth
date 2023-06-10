7d3b6beb67c8d09f8dff57994541b597c3ecb5d0
TLRKiliann <philogenie@protonmail.com>
2023-06-10 17:38:53 +0200

    tutorial at work

---
+++ vue-project/src/components/Card.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Card',
+  data() {
+    return {
+    }
+  },
+})
+</script>
+
+<template>
+    <slot>Default Slot Card</slot>
+
+    <div class="card--header">
+        <slot name="header"></slot>
+    </div>
+    <div class="card--main">
+        <slot name="main"></slot>
+    </div>
+    <div class="card--footer">
+        <slot name="footer"></slot>
+    </div>
+</template>
+
+<style scoped>
+.card--header {
+
+  border: 1px solid #fff;
+}
+.card--main {
+
+border: 1px solid #fff;
+}
+.card--footer {
+
+border: 1px solid #fff;
+}
+</style>
---
+++ vue-project/src/components/PopupComp.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'PopupComp',
+  data() {
+    return {
+    }
+  },
+  methods: {},
+})
+</script>
+
+<template>
+    <h2>My popup</h2>
+</template>
+
+<style scoped>
+
+</style>
